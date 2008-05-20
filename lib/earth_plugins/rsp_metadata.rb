9aa1e2940c8ce9c09ed69d0fe46aa6fd54dd98c2
MFB <Mohammad@localhost.localdomain>
2008-05-20 19:27:20 +0930

    Add comments for calss and methods

--- lib/earth_plugins/rsp_metadata.rb
+++ lib/earth_plugins/rsp_metadata.rb
@@ -1,17 +1,29 @@
-# TODO: add class comments
+# This class provide methods for managing RSP metadata
+# 
+# Author:: Mohammad Bamogaddam
+
 
 class RspMetadata < EarthPlugin
   
-  # TODO: add comments
-  RspKeys = ["job","sequence","shot"]
+  # constant variable used to save rsp naming convention 
+  RSP_KEYS = ["job","sequence","shot"]
   
-  # TODO: add method comments
+  # the +rsp_keys+ method returns array of rsp keys
   def self.rsp_keys
-    RspKeys
+    RSP_KEYS
   end
   
   
-  # TODO: add method comments
+  # the +file_metadata+ method returns hash of keys and values 
+  # representing the passed file metadata
+  # === Parameters
+  # * _file_ = a file object, represent a record in files table
+  #
+  # === Example
+  #   r = RspMetadata.new
+  #   f1 = Earth::File.find(:first, :conditions => ["name = ?", "pic_003.jpg"])
+  #   r.file_metadata(f1) => {"job" => "", "sequence" => "", "shot" => ""}
+  # 
   def file_metadata(file)
     # Find the parent directory for the given file
     # TODO: catch any exception (record not found, etc)
@@ -31,7 +43,13 @@ class RspMetadata < EarthPlugin
   
   private
   
-  # TODO: add method comments
+  # the +parse_path+ parses a given path looking for keys
+  # returns hash of keys and values
+  # === Parameters
+  # * _path_ = String represent some path (e.g. "rsp/job_001/sequence_003/shot_104")
+  # === Example
+  # parse_path("rsp/job_001/sequence_003/shot_104") => {"job" => "_001", "sequence" => "_003", "shot" => "_104"}
+  #
   def parse_path(path)
     metadata = {}
     # split the path into tokens
@@ -39,18 +57,27 @@ class RspMetadata < EarthPlugin
     # loop all over the tokens looking for keys (or something similar to keys)
     tokens.each do |token|
       # find the index of the key (alike)
-      index = match_one_of(RspKeys, token)
+      index = match_one_of(RSP_KEYS, token)
       if index >= 0
         # extract the value
-        value = extract_value(RspKeys[index], token)
+        value = extract_value(RSP_KEYS[index], token)
         # save key, value pairs
-        metadata[RspKeys[index]] = value
+        metadata[RSP_KEYS[index]] = value
       end
     end
     metadata
   end
   
-  # TODO: add method comments
+  # the +match_one_of+ method search in array of keys and comparing each key with a string (_test_)
+  # if one of the keys is found in _test_ => return its index
+  # if not => return -1
+  # === Parameters
+  # * _keys_ = Array of Strings
+  # * _test_ = String
+  # === Example:
+  # keys = ["job","sequence","shot"]
+  # test = "job_LOR"
+  # match_one_of(keys, test) => 0 (index of "job")
   def match_one_of(keys, test)
     keys.each do |key|
       if test =~ Regexp.new(key)
@@ -60,7 +87,13 @@ class RspMetadata < EarthPlugin
     return -1
   end
   
-  # TODO: add method comments
+  # the +extract_value+ method extracts a key value from a String (_test_)
+  # returns a String represent the value of the _key_ in _test_
+  # === Pararmeters
+  # * _key_ = String (e.g. "job")
+  # * _test = String (e.g. "job_LOR")
+  # === Example
+  # extract_value("job", "job_LOR") => "_LOR"
   def extract_value(key, test)
     test.sub(Regexp.new(key), "")
   end
