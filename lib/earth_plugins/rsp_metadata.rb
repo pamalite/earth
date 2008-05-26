399131489a67452c8852d306641cd39aa3a87971
Ida <postgres@localhost.localdomain>
2008-05-26 20:44:59 +0930

    I have added the search method into a plugin

--- lib/earth_plugins/rsp_metadata.rb
+++ lib/earth_plugins/rsp_metadata.rb
@@ -1,6 +1,6 @@
 # This class provide methods for managing RSP metadata
 #
-# Author:: Mohammad Bamogaddam ,Qing Yang
+# Author:: Mohammad Bamogaddam
  
  
 class RspMetadata < EarthPlugin
@@ -38,39 +38,7 @@ class RspMetadata < EarthPlugin
   
   
   
-  # TODO: add method comments
-  # The search_by(key,value) method is writen by QING Yang
-  
-  # Both key and value are from the input of view, which are entered by users.
-  # If key and value are not blank, we find out all the files which include the key and value pairs.
-  # It means if we assume the key is "job" and the value is "A",then the method return all the files 
-  # which have the key and value pairs like ("job","A").
-  
-  
-   
-   def search_by(key,value)
-      result_set_files=Array.new   
-      rsp_name=value
-      rsp_name="*" if rsp_name.blank?
-         
-      rsp_key=key
-      rsp_key="*" if rsp_key.blank?
-      
-      if rsp_name!="*"&& rsp_key!="*"
-        
-        mkvps=Earth::MetadataKeyValuePair.find(:all,:conditions=>
-                                                  ["key LIKE ? and value LIKE ?",key,"%"+value+"%"])
-        mkvps.each do |m|
-          id=m.file_id
-          file=Earth::File.find(id)   
-          result_set_files.push(file)
-        end
-      end
-       
-                                                                
-       return result_set_files
-        
-   end
+
    
    
   private
---
+++ lib/earth_plugins/search_by_kv_pairs.rb
@@ -0,0 +1,42 @@
+
+# Author::Qing Yang
+ 
+
+class SearchByKVPairs < EarthPlugin
+
+  # TODO: add method comments
+  # The search_by(key,value) method is writen by QING Yang
+  
+  # Both key and value are from the input of view, which are entered by users.
+  # If key and value are not blank, we find out all the files which include the key and value pairs.
+  # It means if we assume the key is "job" and the value is "A",then the method return all the files 
+  # which have the key and value pairs like ("job","A").
+  
+  
+   
+   def search_by(key,value)
+      result_set_files=Array.new   
+      rsp_name=value
+      rsp_name="*" if rsp_name.blank?
+         
+      rsp_key=key
+      rsp_key="*" if rsp_key.blank?
+      
+      if rsp_name!="*"&& rsp_key!="*"
+        
+        mkvps=Earth::MetadataKeyValuePair.find(:all,:conditions=>
+                                                  ["key LIKE ? and value LIKE ?",key,"%"+value+"%"])
+        mkvps.each do |m|
+          id=m.file_id
+          file=Earth::File.find(id)   
+          result_set_files.push(file)
+        end
+      end
+       
+                                                                
+       return result_set_files
+        
+   end
+
+
+end
