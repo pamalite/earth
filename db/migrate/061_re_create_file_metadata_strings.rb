dfe7bbee30dfd63dabe8e95bdd589a952720cf21
MFB <Mohammad@localhost.localdomain>
2008-08-19 06:25:29 +0930

    correct the migration file

--- db/migrate/061_re_create_file_metadata_strings.rb
+++ db/migrate/061_re_create_file_metadata_strings.rb
@@ -1,6 +1,10 @@
 class ReCreateFileMetadataStrings < ActiveRecord::Migration
   def self.up
+    #drop the table
     drop_table :file_metadata_strings
+    #TODO delete all records from metadata_strings
+    
+    #re-create the table with a different name
     create_table :files_metadata_strings, :id => false, :force => true do |t|
       t.column :file_id, :integer
       t.column :metadata_string_id, :integer
@@ -10,7 +14,11 @@ class ReCreateFileMetadataStrings < ActiveRecord::Migration
   end
 
   def self.down
+    #drop the table
     drop_table :files_metadata_strings
+    #TODO delete all records from metadata_strings
+    
+    #re-create the table with the old name
     create_table :file_metadata_strings, :id => false, :force => true do |t|
       t.column :file_id, :integer, :null => false
       t.column :metadata_string_id, :integer
