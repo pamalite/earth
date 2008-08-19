a3b4991d336636978fd12a6a3c7845e853f387f7
MFB <Mohammad@localhost.localdomain>
2008-08-20 05:54:43 +0930

    redesign saving metadata

--- lib/earth_plugins/metadata.rb
+++ lib/earth_plugins/metadata.rb
@@ -5,7 +5,9 @@
 # Author:: Mohammad Bamogaddam, Ming
 
 class Metadata < EarthPlugin
-  
+  #TODO abstract methods: 
+  # 1- file_metadata(file), 
+  # 2- create_metadata(file, attributes),
   
   # the +save_file_metadata+ save metadata for a given file
   # if there is already metdata for this file it will be deleted and the new ones will be saved
@@ -14,6 +16,12 @@ class Metadata < EarthPlugin
   #            this method will save metadata for this file
   # TODO we may create a new method to add metadata to the existing ones
   # TODO we may send the plug-in reference which should be used to extract metadata, currently use rsp_metadata
+  # TODO OR we could create the rsp_metadata (or any other metadata plug-ins) as subclass of this one
+  # TODO and define the method (file_metadata) here as an abstract method(i.e. it should be implemented in any subclasses)
+  # the plug-in should give the metadata in a hash form: {key => value, key2 => value2,,,}
+  # where: the (key) should corresponds to an attribute_name in metadata_attributes table
+  # examples of metadata:
+  # {}
   def self.save_file_metadata(file)
     
     #delete any old metadata for this file
@@ -22,11 +30,21 @@ class Metadata < EarthPlugin
     rspmeta = RspMetadata.new
     # extract metadata for the given file using another plug-in
     # TODO the used class to extract metadata could be changed to a passed parameter. so it will be more generic
+    # TODO OR we could create the rsp_metadata (or any other metadata plug-ins) as subclass of this one
+    # TODO and the method (file_metadata) defined here as abstract method(i.e. it should be implemented in any subclasses) 
     metadata = rspmeta.file_metadata(file)
-          
+    
+    #bring the corresponding attribute Ids and types
+    #create a array in the form: 
+    # [{:attribute_id => ??, :attribute_type => ??, :value => ??}, {}, {},,,]
+    metadata_with_att = metadata_with_attributes(metadata)
+    
     # save file metdata
-    for i in 0..metadata.size-1 do      
-      file.metadata_key_value_pairs.create(:key => metadata.keys[i],  :value => metadata.values[i])
+    for i in 0..metadata_with_att.size-1 do 
+      #TODO (OLD, delete) file.metadata_key_value_pairs.create(:key => metadata.keys[i],  :value => metadata.values[i])
+      
+      #TODO use the abstract method create_metadata()
+      file.metadata_strings.create(:value => metadata_with_att[i][:value], :metadata_attribute_id => metadata_with_att[i][:attribute_id])
     end
   end
   
@@ -62,6 +80,20 @@ class Metadata < EarthPlugin
     for i in 0..files.size-1 do
       self.delete_file_metadata(files[i])
     end
+  end
+  
+  
+  private
+  #TODO comments
+  def metadata_with_attributes(metadata)
+    metadata_with_att = Array.new
+    for i in 0..metadata.size-1 do
+      attribute = Earth::MetadataAttribute.find_by_name(metadata.keys[i])
+      unless attribute.nil?
+        metadata_with_att << {:attribute_id => attribute.id, :attribute_type => attribute.metadata_type, :value => metadata.values[i]}
+      end
+    end
+    metadata_with_att
   end
   
 end
