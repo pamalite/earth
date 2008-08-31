0ba5237b94efa368ba0ff21d056900ee15f61a90
MFB <Mohammad@localhost.localdomain>
2008-09-01 00:24:10 +0930

    add method column to the plugin_descriptors table

---
+++ db/migrate/064_add_method_to_plugins.rb
@@ -0,0 +1,9 @@
+class AddMethodToPlugins < ActiveRecord::Migration
+  def self.up
+    add_column :plugin_descriptors,:method, :string
+  end
+
+  def self.down
+    remove_column :plugin_descriptors, :method
+  end
+end
