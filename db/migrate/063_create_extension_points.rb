9cfe588585e49ddc8c3ba68d66edec7bb5b41449
MFB <Mohammad@localhost.localdomain>
2008-09-01 00:17:52 +0930

    modify the migration file to deal with null value records in the plugin_descriptors table

--- db/migrate/063_create_extension_points.rb
+++ db/migrate/063_create_extension_points.rb
@@ -8,24 +8,41 @@ class CreateExtensionPoints < ActiveRecord::Migration
       t.column :description, :text, :null => false
     end
     
-    #add the first extension point which is located at Earthd
-    Earth::ExtensionPoint.create :name => "main_loop", :host_plugin => "Earthd", :description => "extension point at the main loop in the earthd script"
-    
-    #TODO this is temporarily until we read the code from the database
     #add a foreign key to the plug-in descriptor table
     add_column :plugin_descriptors, :extension_point_id, :integer
+    
+    
+    #TODO this is temporarily until we read the code from the database
     change_column :plugin_descriptors, :code, :binary, :null => true
     change_column :plugin_descriptors, :sha1_signature, :binary, :null => true
+    
+    
+    #add the first extension point which is located at Earthd
+    ext = Earth::ExtensionPoint.create :name => "main_loop", :host_plugin => "Earthd", :description => "extension point at the main loop in the earthd script"
+    #add file_monitor as a plugin for Earthd
+    ext.reload
+    #TODO here we should upload the code to the database
+    ext.plugin_descriptors.create :name => "FileMonitor", :version => 1
+
     
   end
 
   def self.down
     drop_table :extension_points
-    #TODO this is temporarily until we read the code from the database
+    
     #remove the foreign key from the plug-in descriptor table
     remove_column :plugin_descriptors, :extension_point_id
+    
+    
+    #TODO this is temporarily until we read the code from the database
     #remove all columns with null values in the code or sha1_signature
-    Earth::PluginDescriptor.delete_all("code = NULL OR sha1_signature = NULL")
+    #I do not know why this does not work!!! so, I just delete all the plugins
+    #all_null_plugins = Earth::PluginDescriptor.find(:all,:conditions => ["code = ? OR sha1_signature = ?",nil,nil])
+    all_null_plugins = Earth::PluginDescriptor.find(:all)
+    for p in all_null_plugins do
+      p.destroy
+    end
+    
     change_column :plugin_descriptors, :code, :binary, :null => false
     change_column :plugin_descriptors, :sha1_signature, :binary, :null => false    
   end
