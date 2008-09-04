36978c50b4dfc63477edb2f4b42d831060baa8f9
MFB <Mohammad@localhost.localdomain>
2008-09-05 06:26:51 +0930

    dd TODO comments

--- db/migrate/064_add_method_to_plugins.rb
+++ db/migrate/064_add_method_to_plugins.rb
@@ -3,10 +3,13 @@ class AddMethodToPlugins < ActiveRecord::Migration
     add_column :plugin_descriptors,:method, :string
     #remove unique name constraint from plugin_descriptors
     execute "ALTER TABLE plugin_descriptors DROP CONSTRAINT plugin_descriptors_unique_name"
+    #TODO create constraint that name + method should be unique in plugin_descriptors
   end
 
   def self.down
     remove_column :plugin_descriptors, :method
+    
+    #TODO remove constraint that name + method should be unique in plugin_descriptors
     
     #add unique name constraint from plugin_descriptors
     execute "ALTER TABLE plugin_descriptors ADD CONSTRAINT plugin_descriptors_unique_name UNIQUE (name)"
