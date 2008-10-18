22b584ab12161af0c6a5cd7668957daaf5ac2f25
MFB <Mohammad@localhost.localdomain>
2008-10-19 02:44:35 +1030

    extension points will be added as part from the installation process

--- db/migrate/064_add_file_monitor_extension_points_and_add_method_column.rb
+++ db/migrate/064_add_file_monitor_extension_points_and_add_method_column.rb
@@ -1,10 +1,6 @@
 class AddFileMonitorExtensionPointsAndAddMethodColumn < ActiveRecord::Migration
   def self.up
     add_column :plugin_descriptors, :method, :string
-    
-    Earth::ExtensionPoint.create :name => "add_file", :host_plugin => "FileMonitor", :description => "when a file is added"
-    Earth::ExtensionPoint.create :name => "delete_file", :host_plugin => "FileMonitor", :description => "before a file is deleted"
-    Earth::ExtensionPoint.create :name => "delete_dir", :host_plugin => "FileMonitor", :description => "before a directory is deleted"
   end
 
   def self.down
