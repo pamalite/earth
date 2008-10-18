f803e612ac5c47ffa476570a537380365d7b85d7
MFB <Mohammad@localhost.localdomain>
2008-10-19 02:47:38 +1030

    change migration file name

--- lib/earth_plugin_interface/extensions.rb
+++ lib/earth_plugin_interface/extensions.rb
@@ -48,6 +48,10 @@ module Extensions
     eval name + '.new'
   end
   
+  def find_related_extension_points(host_plugin_name)
+    Earth::ExtensionPoint.find(:all, :conditions => {:host_plugin => plugin_name})
+  end
+  
   private
   
   def extract_plugin_name(name)
