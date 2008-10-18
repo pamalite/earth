ab698ee229d29e7770a4bc50944bfc338a4481c9
MFB <Mohammad@localhost.localdomain>
2008-10-19 06:05:47 +1030

    change metadata_attributes table. - Now, any new metadata attributes will be added as part of the installation of new plugins (using method migration_up) - plugin_descriptor_id column will be used as foreign key

--- db/migrate/062_add_metadata_type_to_metadata_attributes_and_remove_foreign_key.rb
+++ db/migrate/062_add_metadata_type_to_metadata_attributes_and_remove_foreign_key.rb
@@ -1,17 +1,17 @@
 class AddMetadataTypeToMetadataAttributesAndRemoveForeignKey < ActiveRecord::Migration
   def self.up
     add_column :metadata_attributes, :metadata_type, :string
-    change_column :metadata_attributes, :plugin_descriptor_id, :integer , :null => true
-    remove_foreign_key :metadata_attributes, :metadata_attribute_plugin_descriptor_id_fk
+    #OLD stuff. was for testing metadata_attributes table
+    #change_column :metadata_attributes, :plugin_descriptor_id, :integer , :null => true
+    #remove_foreign_key :metadata_attributes, :metadata_attribute_plugin_descriptor_id_fk
   end
 
   def self.down
     remove_column :metadata_attributes, :metadata_type
-    #TODO delete all rows where plugin_descriptor_id is is null
     
-    #TODO change column plugin_descriptor_id to not accept null values
+    
+    #OLD stuff. was for testing metadata_attributes table
     #change_column :metadata_attributes, :plugin_descriptor_id, :integer , :null => false
-    
-    add_foreign_key :metadata_attributes, :plugin_descriptor_id, :plugin_descriptors, :id, {:name => "metadata_attribute_plugin_descriptor_id_fk"}
+    #add_foreign_key :metadata_attributes, :plugin_descriptor_id, :plugin_descriptors, :id, {:name => "metadata_attribute_plugin_descriptor_id_fk"}
   end
 end
