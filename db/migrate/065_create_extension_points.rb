b0d3c77061d4e04b9de89ac29361f9f1ffbe5c78
MFB <Mohammad@localhost.localdomain>
2008-10-27 03:58:27 +1030

    correct the migration file so it will not affect the code column data type

--- db/migrate/065_create_extension_points.rb
+++ db/migrate/065_create_extension_points.rb
@@ -11,19 +11,10 @@ class CreateExtensionPoints < ActiveRecord::Migration
     #add a foreign key to the plug-in descriptor table
     add_column :plugin_descriptors, :extension_point_id, :integer
     
-    
-    #TODO this is temporarily until we read the code from the database
-    change_column :plugin_descriptors, :code, :binary, :null => true
-    change_column :plugin_descriptors, :sha1_signature, :binary, :null => true
-    
-    
     #add the first extension point which is located at Earthd
     ext = Earth::ExtensionPoint.create :name => "main_loop", :host_plugin => "Earthd", :description => "extension point at the main loop in the earthd script"
     #add file_monitor as a plugin for Earthd
     ext.reload
-    #TODO here we should upload the code to the database
-    #ext.plugin_descriptors.create :name => "FileMonitor", :version => 1
-
     
   end
 
@@ -32,18 +23,7 @@ class CreateExtensionPoints < ActiveRecord::Migration
     
     #remove the foreign key from the plug-in descriptor table
     remove_column :plugin_descriptors, :extension_point_id
+
     
-    
-    #TODO this is temporarily until we read the code from the database
-    #remove all columns with null values in the code or sha1_signature
-    #I do not know why this does not work!!! so, I just delete all the plugins
-    #all_null_plugins = Earth::PluginDescriptor.find(:all,:conditions => ["code = ? OR sha1_signature = ?",nil,nil])
-    all_null_plugins = Earth::PluginDescriptor.find(:all)
-    for p in all_null_plugins do
-      p.destroy
-    end
-    
-    change_column :plugin_descriptors, :code, :binary, :null => false
-    change_column :plugin_descriptors, :sha1_signature, :binary, :null => false    
   end
 end
